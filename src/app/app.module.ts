a1a39137d0f8765d831482a7cea8cf074444095d
Andrew Scott <andrew.scott@US-M0317-5827.attlocal.net>
2020-11-12 09:54:12 -0500

    Incorporated material framework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { CanvasModule } from '../lib/canvas.module';
 
+import { MaterialModule } from './material.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -13,7 +15,9 @@ import { AppComponent } from './app.component';
 	imports: [
 		FormsModule,
 		BrowserModule,
-		CanvasModule
+		BrowserAnimationsModule,
+		CanvasModule,
+		MaterialModule
 	],
 	providers: [],
 	bootstrap: [
